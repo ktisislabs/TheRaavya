abb84e367b924d79c4a7b01c827ba6a93be70d1c
adixb <aditya.bhattacharjee706@gmail.com>
2024-11-28 14:11:07 +0530

    updated code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import HomeImg from '../Assets/Homepage.jpg';
 import NameImage from '../Assets/text-logo.png';
 import { RxHamburgerMenu } from "react-icons/rx";
 import Mainlogo from '../Assets/Mainlogo.png';
+import { FaAmazon } from "react-icons/fa";
 
 function Home() {
   return (
@@ -31,7 +32,7 @@ function Home() {
         <section
           className="Section-2-container w-screen h-screen bg-center bg-no-repeat bg-cover crazy-fade-in"
         >
-          <div className="text-white text-center flex flex-col items-top justify-top h-full">
+          <div className="text-white text-center flex flex-col items-center justify-center h-full">
             <h1 className="text-9xl font-medium font-poppins text-[#848383] px-12 py-12">
               Premium Furniture Products
             </h1>
@@ -44,12 +45,12 @@ function Home() {
         <section
           className="Section-3-container w-screen h-screen bg-center bg-no-repeat bg-cover crazy-fade-in"
         >
-          <div className="text-white text-center flex flex-col items-top justify-center h-full">
-            <h1 className="text-9xl font-medium font-poppins text-[#848383] px-12 py-12">
-              Timeless Designs
+          <div className="text-white text-center flex flex-col items-bottom justify-bottom h-full">
+            <h1 className="text-9xl font-medium  font-poppins text-[white] px-12 py-12">
+             Premium Home Decor Products
             </h1>
-            <p className="font-poppins text-sm text-[#848383]">
-              <u>EXPLORE COLLECTIONS</u>
+            <p className="font-poppins text-sm text-white">
+              <u>LEARN MORE</u>
             </p>
           </div>
         </section>
@@ -57,13 +58,12 @@ function Home() {
         <section
           className="Section-4-container w-screen h-screen bg-center bg-no-repeat bg-cover crazy-fade-in"
         >
-          <div className="text-white text-center flex flex-col items-top justify-center h-full">
-            <h1 className="text-9xl font-medium font-poppins text-[#848383] px-12 py-12">
-              Contact Us Today
+          <div className="text-white text-center flex flex-col items-center justify-center h-full">
+            <h1 className="text-8xl font-medium font-poppins text-white px-12 py-12">
+            Visit our Amazon store
             </h1>
-            <p className="font-poppins text-sm text-[#848383]">
-              <u>GET IN TOUCH</u>
-            </p>
+            <button className='text-white font-poppins font-bold rounded-full flex items-center gap-5 px-12 py-2 opacity-70 bg-black hover:bg-slate-500'>SHOP NOW <FaAmazon className='text-xl'/></button>
+            
           </div>
         </section>
       </div>
